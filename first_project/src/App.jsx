5586fd4fa51d7e6c9c1053259d3b077986a96d2a
ibrahimohid <ibrahimohid77@gmail.com>
2024-05-15 22:13:46 +0600

    first app added

---
+++ first_project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Blogs from './components/Blogs/Blogs'
+import Home from './components/Home/Home'
+
+
+function App() {
+
+  return (
+    <div>
+      <Home></Home>
+      <Blogs></Blogs>
+    </div>
+  )
+}
+
+export default App
---
+++ first_project/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,10 @@
+import './Blogs.css'
+
+function Blogs() {
+    return (
+        <div>
+        <h1 className="blog-heading">I am Form Blogs Component</h1>
+        </div>
+    )
+}
+export default Blogs
