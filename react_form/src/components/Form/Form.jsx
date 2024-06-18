429e5a79543103321eb0ad49d38d6fd134e3e4d4
ibrahimohid <ibrahimohid77@gmail.com>
2024-06-18 15:55:19 +0600

    form practice

--- react_form/src/components/Form/Form.jsx
+++ react_form/src/components/Form/Form.jsx
@@ -1,41 +1,68 @@
 import { useState } from "react";
 
 function Form() {
-    const [text, setText] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    console.log(text);
-    console.log(email);
+  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const [error, setError] = useState("");
 
-    const SubmitForm = (e)=>{
-        e.preventDefault();
-        console.log(e.target.text.value);
-        console.log(e.target.email.value);
-        console.log(e.target.password.value);
-    }
-    const textInput = (e)=>{
-        setText(e.target.value)
-    }
-
-    const emailInput = (e)=>{
-        setEmail(e.target.value);
-    }
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
     
-    const passwordInput = (e)=>{
-        setPassword(e.target.value)
+    if (password.length < 8) {
+      setError("Please Enter 8 Character");
+    } else {
+      setError("");
+      console.log(text);
+      console.log(email);
+      console.log(password);
     }
+
+  };
 
   return (
     <div className="text-center mt-14">
-    <form onSubmit={SubmitForm} action="">
-    <input onChange={textInput} className="bg-blue-800 mb-4 text-white px-5 py-2" type="text" name="text" id="" placeholder="Enter Your Name" /> <br />
-    <input onChange={emailInput} className="bg-blue-800 mb-4 text-white px-5 py-2" type="email" name="email" id="" placeholder="Enter Your Email"/> <br />
-    <input onChange={passwordInput} className="bg-blue-800 mb-4 text-white px-5 py-2" type="password" name="password" id="" placeholder="Enter Your Password"/> <br />
-    <input className="bg-green-600 text-white font-bold py-2 px-5" type="submit" value="Submit" />
-    </form>
+      <form onSubmit={handleSubmitForm} action="">
+        <input
+          onChange={(e) => setText(e.target.value)}
+          className="bg-blue-800 mb-4 text-white px-5 py-2"
+          type="text"
+          name="text"
+          id=""
+          placeholder="Enter Your Name"
+          required
+        />
+        <br />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          className="bg-blue-800 mb-4 text-white px-5 py-2"
+          type="email"
+          name="email"
+          id=""
+          placeholder="Enter Your Email"
+          required
+        />
+        <br />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          className="bg-blue-800 mb-4 text-white px-5 py-2"
+          type="password"
+          name="password"
+          id=""
+          placeholder="Enter Your Password"
+          required
+        />
+        <br />
+        <small>{error}</small> <br />
+        <input
+          className="bg-green-600 text-white font-bold py-2 px-5"
+          type="submit"
+          value="Submit"
+        />
+      </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
